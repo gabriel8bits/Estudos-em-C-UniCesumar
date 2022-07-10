518f3adc724dd042a73d3758eb447f51440ad536
gabriel8bits <gabrielcpa123@gmail.com>
2022-07-09 21:46:46 -0300

    busca-vetor adicionado

---
+++ 4 - Vetores, Strings, Matrizes e Structs/pesquisa-vetor/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int vetorA[10];
+    int i, acha, busca;
+
+    for (i=0;i<10;i++) {
+
+        printf("Digite o %d elemento: ", i);
+        scanf("%d", &vetorA[i]);
+
+    }
+
+    printf("Informe o elemento que deseja buscar: ");
+    scanf("%d", &busca);
+
+    i = 0;
+    acha = 0;
+
+    while ((acha == 0) && (i < 10)) {
+
+        if (vetorA[i] == busca) {
+            acha = 1;
+        } else {
+            i++;
+        }
+    }
+
+    if (acha == 1) {
+        printf("\nO elemente %d foi achado na posição %d.", busca, i);
+    } else {
+        printf("\nO elemente não foi encontrado.\n");
+    }
+
+    return 0;
+}
