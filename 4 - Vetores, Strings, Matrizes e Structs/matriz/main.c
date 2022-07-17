b9be5bb9254f4b5d669426e4c649c588d1bf1007
gabriel8bits <gabrielcpa123@gmail.com>
2022-07-17 10:03:12 -0300

    matriz adicionado

---
+++ 4 - Vetores, Strings, Matrizes e Structs/matriz/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int matrizA[2][10];
+    int i, j;
+
+    for (i=0;i<2;i++) {
+
+        for (j=0;j<10;j++) {
+
+            printf("Insira elemento da linha %d, coluna %d: ", i, j);
+            scanf("%d", &matrizA[i][j]);
+
+        }
+    }
+
+    for (i=0;i<2;i++) {
+
+        for (j=0;j<10;j++) {
+
+            printf("O elemento da linha %d, coluna %d é: %d\n", i, j, matrizA[i][j]);
+
+        }
+    }
+
+    return 0;
+}
