188d81589a714564f1647ba00bbe845d864f9d5f
gabriel8bits <gabrielcpa123@gmail.com>
2022-07-17 21:48:44 -0300

    cadastro de livros feito

--- 4 - Vetores, Strings, Matrizes e Structs/problema1/main.c
+++ 4 - Vetores, Strings, Matrizes e Structs/problema1/main.c
@@ -25,6 +25,50 @@ int main()
 
     while (op != 5) {
 
+        printf("\n1 - Cadastrar Livros\n");
+        printf("2 - Imprimir Livros Cadastrados\n");
+        printf("3 - Pesquisar Livros Por Códico\n");
+        printf("4 - Ordenar Livros Por Ano\n");
+        printf("5 - Sair\n");
+
+        printf("\nDigite a opção desejada: ");
+        scanf("%d", &op);
+        fflush(stdin);
+
+        if (op == 1) {
+
+            system("clear");
+
+            for (i=0;i<TAM;i++) {
+
+                printf("Digite o códico do livro da %d posição: ", i+1);
+                scanf("%d", &ficha[i].codico);
+                fflush(stdin);
+
+                printf("Digite o titulo do livro da %d posição: ", i+1);
+                scanf("\n%50[^\n]s", &ficha[i].titulo);
+                fflush(stdin);
+
+                printf("Digite o autor do livro da %d posição: ", i+1);
+                scanf("\n%30[^\n]s", &ficha[i].autor);
+                fflush(stdin);
+
+                printf("Digite a área do livro da %d posição: ", i+1);
+                scanf("\n%30[^\n]s", &ficha[i].area);
+                fflush(stdin);
+
+                printf("Informe o ano do livro da %d posição: ", i+1);
+                scanf("%d", &ficha[i].ano);
+                fflush(stdin);
+
+                printf("Informe a editora do livro da %d posição: ", i+1);
+                scanf("\n%30[^\n]s", &ficha[i].editora);
+                fflush(stdin);
+
+                printf("\n");
+
+            }
+        }
     }
 
     return 0;
