1b88b4ff36d4f4791699ae1e294ff7b35acb8693
gabriel8bits <gabrielcpa123@gmail.com>
2022-07-17 21:17:48 -0300

    problema1 adicionado

---
+++ 4 - Vetores, Strings, Matrizes e Structs/problema1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 20
+
+struct livro {
+
+    int codico;
+    char titulo[50];
+    char autor[30];
+    char area[30];
+    int ano;
+    char editora[30];
+
+};
+
+int main()
+{
+    struct livro ficha[TAM];
+    struct livro troca;
+
+    int busca, i, j, acha, op;
+
+    op = 0;
+
+    while (op != 5) {
+
+    }
+
+    return 0;
+}
